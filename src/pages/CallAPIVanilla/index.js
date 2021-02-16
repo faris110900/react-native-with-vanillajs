5b4a38a83c7b23c48b421564d141394a03592b31
faris110900 <faris.riskilail@gmail.com>
2021-02-16 15:07:30 +0700

    axios

--- src/pages/CallAPIVanilla/index.js
+++ src/pages/CallAPIVanilla/index.js
@@ -1,17 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet, Button, Image} from 'react-native';
+import Axios from 'axios';
 
 const CallAPIVanilla = () => {
-  const [dataUsers, setDataUsers] = useState ({
+  const [dataUsers, setDataUsers] = useState({
     avatar: '',
     fist_name: '',
     last_name: '',
     email: '',
   });
 
-  const [dataJobs, setDataJobs] = useState ({
+  const [dataJobs, setDataJobs] = useState({
     name: '',
-    job: ''
+    job: '',
   });
 
   useEffect(() => {
@@ -36,34 +37,63 @@ const CallAPIVanilla = () => {
     //   .then((json) => console.log('post respon: ', json));
   }, []);
 
+  //With VanilaJS
+//   const getData = () => {
+//     fetch('https://reqres.in/api/users/2')
+//       .then((response) => response.json())
+//       .then((json) => {
+//         console.log(json);
+//         setDataUsers(json.data);
+//       });
+//   };
+
+  //With Axios
   const getData = () => {
-    fetch('https://reqres.in/api/users/2')
-      .then((response) => response.json())
-      .then((json) => {
-          console.log(json)
-          setDataUsers(json.data)
-        })
+    Axios.get('https://reqres.in/api/users/2')
+    .then(result => {
+        setDataUsers(result.data.data);
+    })
+    .catch(err => console.log('err: ', err));
   };
 
+  //With VanillaJS
+//   const postData = () => {
+//     const dataForAPI = {
+//       name: 'morpheus',
+//       job: 'leader',
+//     };
+
+//     fetch('https://reqres.in/api/users', {
+//       method: 'POST',
+//       headers: {
+//         'Content-type': 'application/json',
+//       },
+//       body: JSON.stringify(dataForAPI),
+//     })
+//       .then((response) => response.json())
+//       .then((json) => {
+//         console.log('post respon: ', json);
+//         setDataJobs(json);
+//       })
+
+//       Axios.post('https://reqres.in/api/users', dataForAPI)
+//       .then(result => console.log('res: ', result))
+//       .catch(err => console.log('err: ', err))
+//   };
+
+  //With Axios
   const postData = () => {
-      const dataForAPI = {
+    const dataForAPI = {
       name: 'morpheus',
       job: 'leader',
     };
 
-    fetch('https://reqres.in/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(dataForAPI),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-          console.log('post respon: ', json)
-          setDataJobs(json)
-        })
-  }
+      Axios.post('https://reqres.in/api/users', dataForAPI)
+      .then(result => {
+          setDataJobs(result.data)
+      })
+      .catch(err => console.log('err: ', err))
+  };
 
   return (
     <View style={styles.container}>
@@ -71,7 +101,11 @@ const CallAPIVanilla = () => {
       <Button title="GET DATA" onPress={getData} />
 
       <Text>Respon GET DATA</Text>
-      <Image source={{uri: dataUsers.avatar}} style={styles.avatar} />
+
+      {dataUsers.avatar.length > 0 && (
+        <Image source={{uri: dataUsers.avatar}} style={styles.avatar} />
+      )}
+
       <Text>{`${dataUsers.fist_name} ${dataUsers.last_name}`}</Text>
       <Text>{dataUsers.email}</Text>
       <View style={styles.line} />
